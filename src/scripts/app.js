cb499fd8a062c9745c52f263966d80788c853e4f
nstreetman <nicole.streetman@Nicoles-MacBook-Air.local>
2017-02-28 16:46:58 -0500

    rendering basic info to the DOM

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,22 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from "jquery"
+
+let appContainerEl = document.querySelector('#app-container')
+
+// $.getJSON("https://congress.api.sunlightfoundation.com/legislators?callback=?").then(function(serverRes){
+//   console.log(serverRes)
+// })
+// // console.log ("Howdy!")
+
+const AllLegislatorsPage = React.createClass({
+  render: function(){
+    return (
+          <div>
+              <h1>Know Your Legislators</h1>
+          </div>
+        )
+  }
+})
+
+  ReactDOM.render(<AllLegislatorsPage/> , appContainerEl)
