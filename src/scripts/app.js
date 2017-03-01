25efd0f7a06db9fb67d47508dd6872697c788332
nstreetman <nicole.streetman@Nicoles-MacBook-Air.local>
2017-03-01 10:29:40 -0500

    2nd commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,21 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from "jquery"
 
+
 let appContainerEl = document.querySelector('#app-container')
 
-// $.getJSON("https://congress.api.sunlightfoundation.com/legislators?callback=?").then(function(serverRes){
-//   console.log(serverRes)
-// })
-// // console.log ("Howdy!")
 
+$.getJSON("https://congress.api.sunlightfoundation.com/legislators?callback=?").then(function(serverRes){
+  console.log(serverRes)
+})
+
+
+// const IteratedLegislatorObj = React.createClass({
+//   _createLegislatorJSX: function(serverRes) {
+//     let jsxLegislatorObj = serverRes.map(function(legislatorObj){
+//       return jsxLegislatorObj
+//     })
+//   }
+// )}
+// forEach (LegislatorObjectsList, function(legislatorObj) {
+// console.log(legislatorObj)
+//   })
+//   })
+//
+// // const IteratedLegislatorObj = React.createClass({
+// //   forEach (LegislatorObjectsList, function(legislatorObj) {
+// //     console.log(legislatorObj)
+// //   })  })
+//
 const AllLegislatorsPage = React.createClass({
   render: function(){
     return (
           <div>
-              <h1>Know Your Legislators</h1>
+                <div>
+                  <h1>Know Your Legislators</h1>
+                </div>
+                <div><p>dammit</p></div>
+              
+
           </div>
         )
   }
 })
 
-  ReactDOM.render(<AllLegislatorsPage/> , appContainerEl)
+
+
+  ReactDOM.render ( <AllLegislatorsPage/> , appContainerEl )
